1197bd68b867f86f6cecbaeab6ffae32281cb84e
robsondev2024 <165223820+robsondev2024@users.noreply.github.com>
2024-03-27 21:12:24 -0300

    Add files via upload

---
+++ aaaa.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2,soma;
+    scanf("%d %d",&num1,&num2);
+    soma=num1+num2;
+    if(soma>35){
+    	printf("soma=%d",soma);
+	}
+    
+    return 0;
+}
